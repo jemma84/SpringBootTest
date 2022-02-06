d333402569b589580be8cc05e52c9e48f7b68fc7
tatsiana.karpovich <extkht@audatex.com>
2022-02-06 22:08:41 +0300

    remove param annotation

--- src/main/java/com/example/demo/repository/PersonRepository.java
+++ src/main/java/com/example/demo/repository/PersonRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
-        public Person findPersonByLogin(String login);
+        Person findPersonByLogin(String login);
 }
